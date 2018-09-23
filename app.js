b72c6bc0e2843a4b4889a56ad380ca67e4647a7a
ywxv <yuewei1126@gmail.com>
2018-09-23 20:34:34 +0800

    try cloud

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 //app.js
 App({
-
+    onLaunch: function(){
+        wx.cloud.init({
+            env: 'hscards-08625d'
+        })
+        const db = wx.cloud.database()
+        db.collection('cardSet').get({
+            success: function(res){
+                console.log(res)
+            }
+        })
+    }
 })
