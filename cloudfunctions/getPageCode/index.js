3d48efc79c96470d50e23b875d65ac023855ab96
ywxv <yuewei1126@gmail.com>
2018-11-10 23:26:43 +0800

    cloudfunction update

--- cloudfunctions/getPageCode/index.js
+++ cloudfunctions/getPageCode/index.js
@@ -7,11 +7,33 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
   let path = event.pagePath
-
+  // return await cloud.callFunction({
+  //   name: 'getAppToken',
+  //   success: function(res){
+  //     return new Promise((resolve, reject) => {
+  //       request('https://api.weixin.qq.com/wxa/getwxacode?path=' + path + '&access_token=' + res.result.access_token, function (error, response, body) {
+  //         resolve(body)
+  //       })
+  //     })
+  //   }
+  // })
+  let token = await cloud.callFunction({
+    name: 'getAppToken'
+  })
+  return new Promise((resolve, reject) => {
+    request({
+      url: 'https://api.weixin.qq.com/wxa/getwxacode?access_token=' + token.result.access_token,
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: {
+        path: path,
+      //   access_token: token.result.access_token
+      }
+    },
+    function (error, response, body) {
+      resolve(body)
+    })
+  })
 }
-
-request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxb35aa69465f737d4&secret=a0c4edd2afac9db03a8a90ac243a80d3', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-})
